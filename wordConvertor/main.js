b1646b306d233dd2bcf7d6b0f265125b8ba160b6
Tanya von Degurechaff <72034257+Victorthemachine@users.noreply.github.com>
2021-06-01 11:38:58 +0200

    Reupload

--- wordConvertor/main.js
+++ wordConvertor/main.js
@@ -1,14 +1,27 @@
 const fs = require('fs');
+const path = require('path');
 
 function checkEntries() {
     console.log('Checking for files...');
-    const files = fs.readdirSync(__dirname + '/entry');
+    const files = fs.readdirSync(path.join(__dirname, '/entry'));
     if (files.length > 0) {
         files.forEach(el => {
-            let file = fs.readFileSync(__dirname + '/entry/' + el, { encoding: "utf-8" });
-            file = file.replace(/[;]/g, ' → ');
-            file = file.replace(/[}\\n|{\\n]/g, '');
-            fs.writeFileSync(__dirname + '/out/' + ('rewritten_' + el), file);
+            let file = fs.readFileSync(path.join(__dirname, '/entry/', el), { encoding: "utf-8" });
+            file = file.replace(/;/g, ' → ');
+            file = file.replace(/}\n|{\n/g, '');
+            //�?
+            //file = file.replace(/�?/g, )
+            file = file.split('\n')
+            let copy = [];
+            file.forEach(line => {
+                console.log(line.split('.'));
+                if(line.split('.').length > 1) {
+                    copy.push(' ');
+                }
+                copy.push(line);
+            });
+            file = copy.join('\n');
+            fs.writeFileSync(path.join(__dirname, '/out/', ('rewritten_' + el)), file);
         });
         console.log('Finished, check out directory. Ending operation')
     } else {
